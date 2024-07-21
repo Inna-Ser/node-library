74c2323f633a6c67fddd810749878971635f88b5
Inna-Ser <serebrakovainna1@gmail.com>
2024-07-21 08:21:13 -0500

    добавлен корневой запрос

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ const server = http.createServer((request, response) => {
   const isUsersExist = url.searchParams.has("users");
   const isBooksExist = url.searchParams.has("books");
 
+  if(request.url === "/") {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "text/plain");
+    response.end("Hello, world!");
+    return;
+  }
+
   if (isUsersExist) {
     response.statusCode = 200;
     response.statusMessage = "OK";

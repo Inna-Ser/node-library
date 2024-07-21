bfc98b18696ee6bc88e91cb8038d2ee3dccc67e7
Inna-Ser <serebrakovainna1@gmail.com>
2024-07-21 08:41:09 -0500

    добавлен запрос на главную страницу

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const http = require("http");
 const { getUsers, getBooks } = require("./modules/library");
 
-
 const server = http.createServer((request, response) => {
   const url = new URL(request.url, `http://127.0.0.1:3002`);
 
   const isUsersExist = url.searchParams.has("users");
   const isBooksExist = url.searchParams.has("books");
 
- 
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "text/plain");
+    response.end("Hello, world!");
+    return;
+  }
 
   if (isUsersExist) {
     response.statusCode = 200;
